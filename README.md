263c2125b4818fd6ec15773dd148781bf2583aa8
thomas grice <augustus.grice@gmail.com>
2015-11-02 19:28:10 -0500

    added thread joins and stuff

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CSemaphoreMultithreading
 Project 2 for Operating Systems Concepts class concerning Semaphores/multithreading using hotel as analogy.
+
+g++ project2.cpp -lpthread
--- project2.cpp
+++ project2.cpp
@@ -9,19 +9,27 @@ pthread_t thread1; //place threads in global scope
 pthread_t thread2;
 sem_t guestReady;
 sem_t workerReady;
-int main() {
-    sem_init(&guestReady, 0, 2);
-    sem_init(&workerReady, 0, 1);
-	cout<<"hello world";
-	return 0;
-}
-
-void guest() {
+sem_t testSem;
+void *guest(void* param) {
+	cout<<"hello world guest thread";
 	while(true){
 
 	}
 }
 
-void frontdesk() {
+void *frontdesk(void* param) {
+	cout<<"hello world frontdesk thread";
 
+}
+int main() {
+    sem_init(&guestReady, 0, 2);
+    sem_init(&workerReady, 0, 1);
+    sem_init(&testSem, 0, 0);
+    pthread_create(&thread1, 0, guest, NULL);
+    pthread_create(&thread2, 0, frontdesk, NULL);
+	cout<<"hello world";
+	pthread_join(thread1, 0);
+    pthread_join(thread2, 0);
+    sem_wait(&testSem);
+	return 0;
 }
