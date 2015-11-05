5448af348ce53bb225151a4f0aefa3d4da97eada
thomas grice <augustus.grice@gmail.com>
2015-11-04 19:00:18 -0500

    moar changes

--- project2.cpp
+++ project2.cpp
@@ -1,35 +1,61 @@
 #include <iostream>
 #include <iomanip>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <pthread.h> //for POSIX threads
 #include <semaphore.h> //for semaphore control
 using namespace std;
 
-pthread_t thread1; //place threads in global scope
-pthread_t thread2;
+int guests = 0;
+int workers = 0;
+int bellhops = 0;
+int guestsRetired = 0;
+pthread_t guestThreads[25]; //place threads in global scope
+pthread_t workerThreads[2];
+pthread_t bellhopThreads[2];
 sem_t guestReady;
 sem_t workerReady;
-sem_t testSem;
-void *guest(void* param) {
-	cout<<"hello world guest thread";
-	while(true){
+sem_t allGuestsRetired;
 
+void *guest(void* param) {
+	int bags = rand() % 5 + 1;
+	int id = guests++;
+	cout<<bags<<endl;
+	cout<<"Guest "<<id<<" has been created"<<endl;
+	sem_post(&guestReady);
+	sem_wait(&workerReady);
+	if(++guestsRetired >= 25) {
+		sem_post(&allGuestsRetired);		
 	}
 }
 
 void *frontdesk(void* param) {
-	cout<<"hello world frontdesk thread";
-
+	int id = workers++;
+    sem_post(&workerReady);
+    while(guestsRetired < 25){
+		cout<<"Front desk employee "<<id<<" created"<<endl;
+    	sem_wait(&guestReady);
+		cout<<"hello world frontdesk thread"<<endl;
+    	sem_post(&workerReady);
+	}
 }
+
 int main() {
-    sem_init(&guestReady, 0, 2);
-    sem_init(&workerReady, 0, 1);
-    sem_init(&testSem, 0, 0);
-    pthread_create(&thread1, 0, guest, NULL);
-    pthread_create(&thread2, 0, frontdesk, NULL);
-	cout<<"hello world";
-	pthread_join(thread1, 0);
-    pthread_join(thread2, 0);
-    sem_wait(&testSem);
+	cout<<"Simulation starts"<<endl;
+	srand(time(NULL));
+    sem_init(&guestReady, 0, 0);
+    sem_init(&workerReady, 0, 0);
+    sem_init(&allGuestsRetired, 0, 0);
+    for(int i=0; i<25; i++) {
+    	pthread_create(&guestThreads[i], 0, &guest, NULL);   
+    	pthread_detach(guestThreads[i]); 	
+    }
+    for(int i=0; i<2; i++) {
+	    pthread_create(&workerThreads[i], 0, &frontdesk, NULL);
+    	pthread_detach(workerThreads[i]); 	
+    }
+    sem_wait(&allGuestsRetired);
+	cout<<"Simulation ends"<<endl;
 	return 0;
 }
